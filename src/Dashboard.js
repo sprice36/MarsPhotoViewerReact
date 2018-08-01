1cd95528f5d122cc32adc3afdcded4aa10d7461a
Sebastian Price <skphoopa@yahoo.com>
2018-08-01 11:48:13 -0400

    styling and Readme and more functionality

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -49,33 +49,40 @@ handleQueryText = (event) => {
 render(){
   
     return(
-        <div className="App">
-        <div className="solChoose"><p>Choose your Sol but keep in mind: Curiosity: 0-2126, Opportunity: 0-5111, Spirit: 0-2208 </p></div>
-        <TextBar className="sol" sol={this.state.sol} handleQueryText={this.handleQueryText}  />
-        <div className="marsImage">
-        <img src="https://static-41.sinclairstoryline.com/resources/media/595b7746-7b3b-4bf0-bcd3-d68400f3a9ab-large16x9_marsFile.jpg?1532731663007" />
-        </div>
-         <div className="roverChoose"><p>Choose your Rover</p></div>
-        <div className="roverContainer">
-        <div className ="curiosity"> 
-              <img src="https://mars-photos.herokuapp.com/explore/images/Curiosity_rover.jpg" />
-         </div> 
-        <div className ="opportunity"> 
-            <img src="https://mars-photos.herokuapp.com/explore/images/Opportunity_rover.jpg" />
-            </div> 
-        <div className ="spirit"> 
-            <img src="https://mars-photos.herokuapp.com/explore/images/Spirit_rover.jpg" />
-            </div> 
-        </div> 
-        <div className="roverbuttonContainer">
-          
-        </div>
+        <div className="background">
+            <div className="App">
+                <div className="solChoose">
+                    <p>Choose your Sol but keep in mind: Curiosity: 0-2126, Opportunity: 0-5111, Spirit: 0-2208 
+                    </p>
+                </div>
+                <div> 
+                    <TextBar className="sol" sol={this.state.sol} handleQueryText={this.handleQueryText}  />
+                </div>
+                <div className="roverChoose">
+                    <p>Choose your Rover
+                    </p>
+                </div>
+        {/* /<center> */}
+                <div className="roverContainer">
+                    <div className ="curiosity"> 
+                        <img src="https://mars-photos.herokuapp.com/explore/images/Curiosity_rover.jpg" />
+                    </div> 
+                    <div className ="opportunity"> 
+                        <img src="https://mars-photos.herokuapp.com/explore/images/Opportunity_rover.jpg" />
+                    </div> 
+                    <div className ="spirit"> 
+                        <img src="https://mars-photos.herokuapp.com/explore/images/Spirit_rover.jpg" />
+                    </div> 
+                </div> 
+        {/* </center> */}
         <NavBar />
-       <Route path="/" exact={true} component={Landing} /> 
-       <Route path="/curiosity" render={(props) => <Curiosity {...props} sol={this.state.sol}/>  } />
-       <Route path="/opportunity" render={(props) => <Opportunity {...props} sol={this.state.sol}/> } />
-       <Route path="/spirit" render={(props) => <Spirit {...props} sol={this.state.sol} />} />
-       </div>
+            <Route path="/" exact={true} component={Landing} /> 
+            <Route path="/curiosity" render={(props) => <Curiosity {...props} sol={this.state.sol}/>  } />
+            <Route path="/opportunity" render={(props) => <Opportunity {...props} sol={this.state.sol}/> } />
+            <Route path="/spirit" render={(props) => <Spirit {...props} sol={this.state.sol} />} />
+            </div>
+    
+        </div>
     );
 } 
 }
